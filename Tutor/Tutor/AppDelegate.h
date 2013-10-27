6c17fc544f4d9d4dea8bd868f2485fdfec35b267
syzhou <zhouxbbbj1@126.com>
2013-10-28 00:34:45 +0800

    add project fils

---
+++ Tutor/Tutor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tutor
+//
+//  Created by syzhou on 13-10-28.
+//  Copyright (c) 2013年 syzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tutor/Tutor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tutor
+//
+//  Created by syzhou on 13-10-28.
+//  Copyright (c) 2013年 syzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
